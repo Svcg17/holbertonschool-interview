dc01db3d45409d352eeb265296e27a2f496272ba
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-03 15:35:54 -0800

    Handling basic edge cases like checking types and length

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,13 +9,18 @@
 def canUnlockAll(boxes):
     newList = [0]
     i = j = 0
-    for i in newList:
-        for j in boxes[i]:
-            if j not in newList:
-                newList.append(j)
-            else:
-                continue
-    if len(newList) == len(boxes):
+    if len(boxes) == 0:
         return True
-    else:
-        return False
+    if boxes and isinstance(boxes, list):
+        for i in newList:
+            for j in boxes[i]:
+                if j < 0:
+                    return False
+                if j not in newList:
+                    newList.append(j)
+                else:
+                    continue
+        if len(newList) == len(boxes):
+            return True
+        else:
+            return False
