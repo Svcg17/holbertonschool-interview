67a293314102f62cf32abb16b303272c1f331354
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-03 16:15:28 -0800

    Explicitly return false for failed edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,18 +9,20 @@
 def canUnlockAll(boxes):
     newList = [0]
     i = j = 0
+    if not boxes or not isinstance(boxes, list):
+        return False
     if len(boxes) == 0:
         return True
-    if boxes and isinstance(boxes, list):
-        for i in newList:
-            for j in boxes[i]:
-                if j < 0:
-                    return False
-                if j not in newList:
-                    newList.append(j)
-                else:
-                    continue
-        if len(newList) == len(boxes):
-            return True
-        else:
-            return False
+
+    for i in newList:
+        for j in boxes[i]:
+            if not isinstance(j, int) or j < 0 or j > len(boxes):
+                return False
+            if j not in newList:
+                newList.append(j)
+            else:
+                continue
+    if len(newList) == len(boxes):
+        return True
+    else:
+        return False
