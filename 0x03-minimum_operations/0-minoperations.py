a68ca7e1ab8f7e70e37633787d269cb126bd8945
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-24 13:12:00 -0800

    Removing isPrime function, checking directly in main function

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-    calculates the fewest number of operations needed to result in exactly n H characters.
+    calculates the fewest number of operations needed to result in
+    exactly n H characters.
     Args:
         @n: integer
-    Return: the fewest number of operations needed to result in exactly n H characters
+    Return: the fewest number of operations needed to result in n H characters
 """
+
+
 def minOperations(n):
     suma = 0
     if n <= 1:
         return suma
-    elif n % 2 != 0:
-        return n
     else:
         for i in range(2, n + 1):
             while n % i == 0:
                 suma += i
                 n = n // i
-    return suma
-            
+        return suma
+    return n
