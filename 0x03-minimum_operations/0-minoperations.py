51345bcb9fbd0ecd28c73ad9f9b31658552fbf18
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-24 19:57:32 -0800

    Loop in range square root of n

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-    calculates the fewest number of operations needed to result in
-    exactly n H characters.
-    Args:
-        @n: integer
-    Return: the fewest number of operations needed to result in n H characters
-"""
+import math
 
 
 def minOperations(n):
+    """ Calculates the fewest number of operations needed to result in
+        exactly n H characters.
+
+        Args:
+            @n: integer
+
+        Return:
+            the fewest number of operations needed to result in n H characters
+    """
     suma = 0
     if n <= 1:
         return suma
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n) + 1)):
         while n % i == 0:
             suma += i
             n = n // i
