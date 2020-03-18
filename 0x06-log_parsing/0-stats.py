1c1cb4a0e3a840f35d856d9261b97d2aa588c0cf
Sofia Cheung <sofiavictoria17@gmail.com>
2020-03-17 23:39:52 -0700

    Handling ctrl-C signal

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,26 +7,37 @@
         - print the sum of the file sizes
 """
 import sys
+import signal
 
 c = fileSize = 0
 statCount = {}
 
-for line in sys.stdin:
-    split = line.split(" ")
-    if len(split) < 9:
-        continue
-    status = split[7]
-    fileSize += int(split[8])
 
-    if statCount.get(status):
-        statCount[status] += 1
-    else:
-        statCount[status] = 1
+def handleTen(statCount, c, fileSize):
+    for key in sorted(statCount.keys()):
+        sys.stdout.write("{}: {}\n".format(key, statCount[key]))
+    sys.stdout.write("File size: {}\n".format(fileSize))
+    c = fileSize = 0
+    statCount = {}
+    return statCount, c, fileSize
 
-    c += 1
-    if c == 9:
-        for key in sorted(statCount.keys()):
-            sys.stdout.write("{}: {}\n".format(key, statCount[key]))
-        sys.stdout.write("File size: {}\n".format(fileSize))
-        c = fileSize = 0
-        statCount = {}
+
+try:
+    for line in sys.stdin:
+        split = line.split(" ")
+        if len(split) < 9:
+            continue
+        status = split[7]
+        fileSize += int(split[8])
+
+        if statCount.get(status):
+            statCount[status] += 1
+        else:
+            statCount[status] = 1
+
+        c += 1
+        if c == 9:
+            statCount, c, fileSize = handleTen(statCount, c, fileSize)
+
+except KeyboardInterrupt:
+    statCount, c, fileSize = handleTen(statCount, c, fileSize)
