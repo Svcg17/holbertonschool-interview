896d5ab19b23456a81ceab0f4e2a235afe09ae68
Sofia Cheung <sofiavictoria17@gmail.com>
2020-04-07 13:16:51 -0700

    Uploading files

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+def validUTF8(data):
+    counter = 0
+    for num in data:
+        mask = 0b10000000
+        if not counter:
+            while (mask & num):
+                counter += 1
+                mask >>= 1
+            if counter > 4:
+                return False
+            if counter:
+                counter -= 1
+                if counter == 0:
+                    return False
+        elif counter:
+            if num >> 6 == 2:
+                counter -= 1
+            else:
+                return False
+    if not counter:
+        return True
+    else:
+        return False
