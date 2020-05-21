a157eae853e6fbcf159b02299aa889181e87f935
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-20 22:28:03 -0700

    Fixing betty style errors

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -4,20 +4,20 @@
  * create_node - creates an avl_t node
  * @val: value of the node
  * @parent: parent of the new node
- * 
+ *
  * Return: pointer to the newly created node
  */
 avl_t *create_node(int val, avl_t *parent)
 {
-    avl_t *new = malloc(sizeof(avl_t));
+	avl_t *new = malloc(sizeof(avl_t));
 
-    if (!new)
-        return NULL;
-    new->n = val;
-    new->left = NULL;
-    new->right = NULL;
-    new->parent = parent;
-    return (new);
+	if (!new)
+		return (NULL);
+	new->n = val;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	return (new);
 }
 
 /**
@@ -32,16 +32,16 @@ avl_t *create_node(int val, avl_t *parent)
  */
 avl_t *construct_tree(avl_t *parent, int *array, int l, int r)
 {
-    avl_t *new;
-    int m = (r - l) / 2 + l;
+	avl_t *new;
+	int m = (r - l) / 2 + l;
 
-    if (l > r)
-        return NULL;
-    new = create_node(array[m], parent);
-    new->left = construct_tree(new, array, l, m - 1);
-    new->right= construct_tree(new, array, m + 1, r);
+	if (l > r)
+		return (NULL);
+	new = create_node(array[m], parent);
+	new->left = construct_tree(new, array, l, m - 1);
+	new->right = construct_tree(new, array, m + 1, r);
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -49,13 +49,13 @@ avl_t *construct_tree(avl_t *parent, int *array, int l, int r)
  * @array: pointer to the first element of the array to be converted
  * @size: number of element in the array
  * Return: a pointer to the root node of the created AVL tree or NULL
-*/
+ */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *root = NULL;
-    int l = 0;
-    int r = size - 1;
-    
-    root = construct_tree(root, array, l, r);
-    return (root);
+	avl_t *root = NULL;
+	int l = 0;
+	int r = size - 1;
+
+	root = construct_tree(root, array, l, r);
+	return (root);
 }
