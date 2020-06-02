3fdea7adff383aca4aee0e55650d5754bbe5c69c
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-02 07:10:28 +0000

    Code now meets requirement of displaying each name in order by using async/await and promises

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,32 @@
 #!/usr/bin/node
+
 const request = require('request');
-if (process.argv.length === 3) {
-  const id = process.argv[2];
+
+async function sw (id) {
   const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-  request(url, function (err, response, body) {
+  request(url, async function (err, response, body) {
     if (err) {
       console.log(err);
     } else {
       for (const ch of JSON.parse(body).characters) {
-        request(ch, function (err, response, body) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
-        });
+        const ret = () => {
+          return new Promise((resolve, reject) => {
+            request(ch, function (err, response, body) {
+              if (err) {
+                console.log(err);
+              } else {
+                resolve(JSON.parse(body).name);
+              }
+            });
+          });
+        };
+        console.log(await ret());
       }
     }
   });
+}
+
+if (process.argv.length === 3) {
+  sw(process.argv[2]);
 }
