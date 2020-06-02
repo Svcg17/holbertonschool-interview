f2f8c9308a35d87e6598fa5dd9f947acd00ecb63
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-01 21:36:56 -0700

    Handling number of args

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+if (process.argv.length === 3) {
+  const id = process.argv[2];
+  const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    for (const ch of JSON.parse(body).characters) {
-      request(ch, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      for (const ch of JSON.parse(body).characters) {
+        request(ch, function (err, response, body) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(JSON.parse(body).name);
+          }
+        });
+      }
     }
-  }
-});
+  });
+}
