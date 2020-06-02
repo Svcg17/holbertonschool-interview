730fc352cd7cfbefd53496af8a8f2bd9a883b578
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-01 21:28:13 -0700

    Updating url

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = `http://swapi.co/api/films/${id}`;
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
 request(url, function (err, response, body) {
   if (err) {
