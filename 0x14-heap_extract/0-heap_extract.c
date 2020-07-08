5b6199fe3e6fe50b306a4fdcf082ddbc1cb861d0
Sofia Cheung <sofiavictoria17@gmail.com>
2020-07-08 15:06:55 -0700

    Uploading init files

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to the root of the heap
+ * Return: the value stored in the root node, or 0.
+ */
+int heap_extract(heap_t **root)
+{
+}
