9b02ae266d7b0c33a009cfc55370c907296a16f1
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-23 23:54:59 -0700

    Creating functions that check if a tree is a valid AVL tree

--- 0x1D-avl_trees/0-main.c
+++ 0x1D-avl_trees/0-main.c
@@ -3,6 +3,25 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: pointer to the parent node of the node to create.
+ * @value: value to put in the new node.
+ * Return: a pointer to the new node, or NULL on failure.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
+}
+
+/**
  * basic_tree - Build a basic binary tree
  *
  * Return: A pointer to the created tree
